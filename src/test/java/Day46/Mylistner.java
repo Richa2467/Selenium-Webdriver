a806b5114bac3f710dc0c487397af1731865e83c
Richa2467 <richa.tyagi@acldigital.com>
2025-09-24 14:51:05 +0530

    changes

--- src/test/java/Day46/Mylistner.java
+++ src/test/java/Day46/Mylistner.java
@@ -1,5 +1,40 @@
 package Day46;
 
-public class Mylistner {
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
 
+public class Mylistner  implements ITestListener{
+	
+	public void onStart(ITestContext  context)     //Once before the execution strated.
+	{
+		System.out.println("Test execution is Started..... ");
+	}
+	
+	public void onTestStart(ITestResult   result)  //before every @Test 
+	{
+		System.out.println("test started....");
+	}
+	
+	public void onTestSuccess(ITestResult   result)
+	{
+		System.out.println("Test Passed....");
+	}
+	
+	public void onTestFailure(ITestResult   result)
+	{
+		System.out.println("Test Failed....");
+	}
+	
+	public void onTestSkip(ITestResult   result)
+	{
+		System.out.println("Test skiped....");
+	}
+	
+
+	public void onFinish(ITestContext   context)
+	{
+		System.out.println("Test execution is completed...");
+	
+	}
 }
